681f9910f2e7b409c67253c8d58e3ccd89f624de
popi popi po po pi po <diana.bakieva69@gmail.com>
2025-10-23 18:34:05 +0500

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+struct Message
+{
+    public bool Result { get; set; }
+    public int Data { get; set; }
+}
+
+class PipeServer
+{
+    static void Main(string[] args)
+    {
+        using (var pipeServer = new NamedPipeServerStream("C_Sharp1"))
+        {
+            Console.WriteLine("Server is waiting for a connection...");
+            pipeServer.WaitForConnection(); 
+
+            Message receivedMessage = ReadMessage(pipeServer);
+            Console.WriteLine("Result = {0}, Data = {1}", receivedMessage.Result, receivedMessage.Data);
+
+            receivedMessage.Result = true;
+            WriteMessage(pipeServer, receivedMessage);
+        }
+
+        Console.WriteLine("Server's work is done");
+        Console.ReadLine();
+    }
+
+    static Message ReadMessage(NamedPipeServerStream pipeStream)
+    {
+        byte[] buffer = new byte[Unsafe.SizeOf<Message>()];
+        pipeStream.Read(buffer, 0, buffer.Length);
+        return MemoryMarshal.Read<Message>(buffer);
+    }
+
+    static void WriteMessage(NamedPipeServerStream pipeStream, Message message)
+    {
+        byte[] buffer = new byte[Unsafe.SizeOf<Message>()];
+
+        MemoryMarshal.Write(buffer, ref message);
+
+        pipeStream.Write(buffer, 0, buffer.Length);
+        pipeStream.Flush();
+    }
+}
